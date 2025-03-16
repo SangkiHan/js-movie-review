f552b368e1aedd3106d9db55d1778719c072f329
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-03-16 18:06:03 +0900

    feat: baseurl 묶기

--- src/api/MovieApiInfomation.js
+++ src/api/MovieApiInfomation.js
@@ -1,5 +1,5 @@
-export const getMoviePopularApiUrl =
-  "https://api.themoviedb.org/3/discover/movie";
-export const getMovieSearchKeyowrdApiUrl =
-  "https://api.themoviedb.org/3/search/movie";
+const movieApi = "https://api.themoviedb.org/3";
+
+export const getMoviePopularApiUrl = movieApi + "/discover/movie";
+export const getMovieSearchKeyowrdApiUrl = movieApi + "/search/movie";
 export const movieApiKey = import.meta.env.VITE_API_KEY;
