27ffa528c5826aa21793c9a47c004eca7fc84134
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-03-16 18:06:29 +0900

    test: 탭 잘뜨는지 cypress 테스트코드

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,6 +8,24 @@ describe("Header 테스트", () => {
   });
 });
 
+describe("Nav 테스트", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/");
+  });
+
+  it("Nav 떴는지 확인", () => {
+    cy.get("h2").should("contain", "지금 인기있는 영화");
+  });
+
+  it("탭 메뉴가 제대로 떴는지 확인", () => {
+    cy.get(".tab-item").should("have.length", 4);
+
+    cy.get(".tab-item").eq(0).should("contain", "상영중");
+    cy.get(".tab-item").eq(1).should("contain", "인기순");
+    cy.get(".tab-item").eq(2).should("contain", "평점순");
+    cy.get(".tab-item").eq(3).should("contain", "상영예정");
+  });
+});
 describe("영화목록", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173/");
