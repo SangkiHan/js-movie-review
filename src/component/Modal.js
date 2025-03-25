77a0bcc7e0e4df89ce8bf4fbdcf9536a7d34a112
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-03-26 01:48:58 +0900

    feat: 영화 상세 모달창 추가

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -1,8 +1,17 @@
+import { starMessage } from "../domain/StarMessage";
+import { starImage } from "../Image";
+
 export function openModal(movieInstance) {
   const body = document.body;
   const div = document.createElement("div");
   div.classList.add("modal-background");
-  div.id.add("modalBackground");
+  div.classList.add("active");
+  div.id = "modalBackground";
+
+  const starImagePath =
+    movieInstance.voteAverage === "0.0"
+      ? starImage["empty"]
+      : starImage["filled"];
 
   div.innerHTML = /*html*/ `
     <div class="modal">
@@ -27,8 +36,12 @@ export function openModal(movieInstance) {
                 >
             </p>
             <hr />
+            <h3>내 평점</h3>
+            <div class="my-rating">${renderStars(movieInstance.myVote)}</div>
+            <hr />
+            <h3>줄거리</h3>
             <p class="detail">
-                ${movieInstance.overview}
+                ${movieInstance.overView}
             </p>
             </div>
         </div>
@@ -36,6 +49,47 @@ export function openModal(movieInstance) {
     `;
 
   body.appendChild(div);
+  clickCloseModal();
+  clickStar(movieInstance);
 }
 
-export function closeModal() {}
+function clickCloseModal() {
+  const closeButton = document.querySelector(".close-modal");
+  closeButton.addEventListener("click", () => {
+    closeModal();
+  });
+}
+
+function closeModal() {
+  const div = document.querySelector(".modal-background");
+  div.remove();
+}
+
+function renderStars(rate) {
+  return (
+    Array.from({ length: 5 }, (_, index) => {
+      return `<img src="${
+        index < rate ? starImage["filled"] : starImage["empty"]
+      }" class="star" 
+    data-index="${index}"/>`;
+    }).join("") + starMessage[rate]
+  );
+}
+
+function clickStar(movieInstance) {
+  const starElements = document.querySelectorAll(".my-rating .star");
+  starElements.forEach((star) => {
+    star.addEventListener("click", (event) => {
+      const clickedIndex = event.target.dataset.index;
+      const newRate = parseInt(clickedIndex) + 1;
+      movieInstance.updateMyVote(newRate);
+      updateRating(newRate, movieInstance);
+    });
+  });
+}
+
+function updateRating(newRate, movieInstance) {
+  const myRatingContainer = document.querySelector(".my-rating");
+  myRatingContainer.innerHTML = renderStars(newRate);
+  clickStar(movieInstance);
+}
